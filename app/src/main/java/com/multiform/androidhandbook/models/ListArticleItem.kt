3695a071397348105dfc682cf625cff0417ea4c3
Multiform <nikitos1ru@gmail.com>
2020-11-23 19:18:04 +0300

    Fixed onClick for RecyclerView

--- app/src/main/java/com/multiform/androidhandbook/models/ListArticleItem.kt
+++ app/src/main/java/com/multiform/androidhandbook/models/ListArticleItem.kt
@@ -7,6 +7,7 @@ import com.multiform.androidhandbook.utils.openArticle
 import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
 import com.xwray.groupie.kotlinandroidextensions.Item
 import kotlinx.android.synthetic.main.item_list.*
+import kotlinx.android.synthetic.main.item_list.view.*
 import java.io.Serializable
 
 class ListArticleItem (
@@ -18,22 +19,23 @@ class ListArticleItem (
 
     override fun getLayout(): Int = R.layout.item_list
 
+    override fun createViewHolder(itemView: View): GroupieViewHolder {
+        itemView.titleListItem.setOnClickListener {
+            openArticle(this)
+        }
+        itemView.descriptionBtnListItem.setOnClickListener {
+            if (itemView.descriptionTextListItem.visibility == View.GONE) {
+                itemView.descriptionTextListItem.visibility = View.VISIBLE
+            } else {
+                itemView.descriptionTextListItem.visibility = View.GONE
+            }
+        }
+        return super.createViewHolder(itemView)
+    }
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
         viewHolder.titleListItem.text = title
         if (isBold) viewHolder.titleListItem.setTypeface(Typeface.DEFAULT, Typeface.BOLD)
         viewHolder.imgListItem.setImageResource(res_ic)
         viewHolder.descriptionTextListItem.text = text
-
-        viewHolder.descriptionBtnListItem.setOnClickListener {
-            if (viewHolder.descriptionTextListItem.visibility == View.GONE) {
-                viewHolder.descriptionTextListItem.visibility = View.VISIBLE
-            } else {
-                viewHolder.descriptionTextListItem.visibility = View.GONE
-            }
-        }
-
-        viewHolder.titleListItem.setOnClickListener {
-            openArticle(this)
-        }
     }
 }
